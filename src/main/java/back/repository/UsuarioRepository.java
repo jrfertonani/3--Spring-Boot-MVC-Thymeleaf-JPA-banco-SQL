22115e3f0bec543a5fb3da4080546facba8e4efc
Ademir <ademirjuniorfertonani@gmail.com>
2026-02-11 18:41:49 -0300

    repository service Usuario

---
+++ src/main/java/back/repository/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package back.repository;
+
+import back.controller.Usuario;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Transactional
+public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+
+    @Query("select u from Usuario u where u.login = ?1")
+    Usuario findUserByLogin(String Login);
+
+}
---
+++ src/main/java/back/security/ImplementacaoUserDatailService.java
@@ -0,0 +1,21 @@
+package back.security;
+
+import back.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImplementacaoUserDatailService implements UserDetailsService{
+
+
+    @Autowired
+    private UsuarioRepository usuarioRepository
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
